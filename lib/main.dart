75d5262198e8813d118d54ebdc5038f00a6f1343
Nathaniel Samede <nsamede7@gmail.com>
2025-10-24 10:41:56 +0100

    Remove comment

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,6 @@ void main() {
 class App extends StatelessWidget {
   const App({super.key});
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     title: "Sandwich Shop App",
-  //     home: Scaffold(
-  //       appBar: AppBar(title: const Text("Sandwich Counter")),
-  //       body: Center(
-  //         child: ListView(
-  //           children: [
-  //             for (var i = 0; i < 20; i++)
-  //               Container(
-  //                 width: 300,
-  //                 height: 100,
-  //                 margin: EdgeInsets.all(20),
-  //                 color: Colors.red,
-  //                 child: OrderItemDisplay(3, "BLT"),
-  //               ),
-  //           ],
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
