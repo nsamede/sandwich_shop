a342f1433bbe3c9e2d5fe03dafc9d1f05ac79f88
Nathaniel Samede <nsamede7@gmail.com>
2025-10-24 09:34:25 +0100

    Use OrderItemDisplay in App

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class App extends StatelessWidget {
       title: "Sandwich Shop App",
       home: Scaffold(
         appBar: AppBar(title: const Text("Sandwich Counter")),
-        body: const Center(child: Text("Welcome to the Sandwich Shop!")),
+        body: const Center(child: OrderItemDisplay(5, "Footlong")),
       ),
     );
   }
