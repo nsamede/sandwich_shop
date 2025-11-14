0cd0574a0d78ab31197916ca9f4181dd1796df24
Nathaniel Samede <nsamede7@gmail.com>
2025-11-14 15:04:10 +0000

    Add test for sandwich type slider

--- test/views/widget_test.dart
+++ test/views/widget_test.dart
@@ -92,6 +92,14 @@ void main() {
       await tester.pump();
       expect(find.text('Note: Extra mayo'), findsOneWidget);
     });
+
+    testWidgets('updates note with Switch', (WidgetTester tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.textContaining("white footlong sandwich"), findsOneWidget);
+      await tester.tap(find.byType(Switch));
+      await tester.pumpAndSettle();
+      expect(find.textContaining("white six-inch sandwich"), findsOneWidget);
+    });
   });
 
   group('StyledButton', () {
