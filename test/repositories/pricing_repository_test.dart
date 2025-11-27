9a713b311f48817baac81ac7472673a54c821719
nsamede <nsamede7@gmail.com>
2025-11-27 09:59:01 +0000

    begin worksheet5

--- test/repositories/pricing_repository_test.dart
+++ test/repositories/pricing_repository_test.dart
@@ -1,27 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sandwich_shop/repositories/order_repository.dart';
 import 'package:sandwich_shop/repositories/pricing_repository.dart';
 
 void main() {
-  group("PricingRepository", () {
-    test("initial price should be 0", () {
-      final orderRepository = OrderRepository(maxQuantity: 5);
-      final pricingRepository = PricingRepository(orderRepository);
-      expect(pricingRepository.price, 0);
+  group('PricingRepository', () {
+    final repository = PricingRepository();
+
+    test('calculates price for single six-inch correctly', () {
+      final price = repository.calculatePrice(quantity: 1, isFootlong: false);
+      expect(price, 7.00);
     });
-    test("adding a footlong sandwich should increase price by 11", () {
-      final orderRepository = OrderRepository(maxQuantity: 5);
-      final pricingRepository = PricingRepository(orderRepository);
-      pricingRepository.isFootlong = true;
-      orderRepository.increment();
-      expect(pricingRepository.price, 11);
+
+    test('calculates price for multiple footlongs correctly', () {
+      final price = repository.calculatePrice(quantity: 3, isFootlong: true);
+      expect(price, 33.00);
     });
-    test("adding a six-inch sandwich should increase price by 7", () {
-      final orderRepository = OrderRepository(maxQuantity: 5);
-      final pricingRepository = PricingRepository(orderRepository);
-      pricingRepository.isFootlong = false;
-      orderRepository.increment();
-      expect(pricingRepository.price, 7);
+
+    test('calculates price for zero quantity as zero', () {
+      final price = repository.calculatePrice(quantity: 0, isFootlong: true);
+      expect(price, 0.00);
     });
   });
 }
